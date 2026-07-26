22a6abc05e71fd42084b927b3d57326684df83b1
Ricardo Jorge <ricardojorgexyz@gmail.com>
2026-07-26 16:20:49 +0100

    fix: light mode close

--- v0/www/components/ui/dialog.tsx
+++ v0/www/components/ui/dialog.tsx
@@ -71,7 +71,7 @@ function DialogContent({
           <DialogPrimitive.Close data-slot="dialog-close" asChild>
             <Button
               variant="ghost"
-              className="absolute top-4 right-4 bg-secondary"
+              className="absolute top-4 right-4 bg-secondary text-foreground"
               size="icon-sm"
             >
               <XIcon
