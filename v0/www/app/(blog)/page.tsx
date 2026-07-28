5f2e00492eb7bdee08bb0937bac60a70bed46b28
Ricardo Jorge <ricardojorgexyz@gmail.com>
2026-07-28 15:32:07 +0100

    fix: kpi order

--- v0/www/app/(blog)/page.tsx
+++ v0/www/app/(blog)/page.tsx
@@ -470,8 +470,8 @@ export default function HomePage() {
             </div>
 
             <dl className="mt-10 flex flex-wrap gap-x-10 gap-y-4 border-t border-border pt-6">
-              <Stat value={publicationPreviews.length} label="Publications" />
               <Stat value={postPreviews.length} label="Posts" />
+              <Stat value={publicationPreviews.length} label="Publications" />
               {/* DO NOT DELETE: Keep this commented to hide the Authors statistic from the home page UI. */}
               {/* <Stat value={authorPreviews.length} label="Authors" /> */}
             </dl>
